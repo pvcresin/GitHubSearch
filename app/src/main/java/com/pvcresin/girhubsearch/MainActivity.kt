da60ad9e9f0f5a5cead8de3936b2ca9e586b8f94
pvcresin <pvcresin0730@gmail.com>
2017-07-05 17:05:06 +0900

    update MainActivityからCustomTabでGitHubの認証ページを開き，リダイレクトでアプリに戻ってcodeを取得

--- app/src/main/java/com/pvcresin/girhubsearch/MainActivity.kt
+++ app/src/main/java/com/pvcresin/girhubsearch/MainActivity.kt
@@ -1,19 +1,44 @@
 package com.pvcresin.girhubsearch
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "Main"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        ClientData(this)
+        val clientData = ClientData(this)
+
+        fun openOAuthPage() {
+            val customTab = CustomTabsIntent.Builder()
+                    .setShowTitle(true)
+                    .enableUrlBarHiding().build()
+            customTab.launchUrl(this,
+                    Uri.parse("https://github.com/login/oauth/authorize?client_id=${clientData.id}"))
+        }
+
+        (findViewById(R.id.button) as Button).setOnClickListener {
+            openOAuthPage()
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        val code = intent?.data?.getQueryParameter("code")
+        Log.d(TAG, "code $code")
     }
 }
+
 
 class ClientData(context: Context) {
     var id: String
