3887ef24790b7bce67f510958af16c8627dccaab
pvcresin <pvcresin0730@gmail.com>
2017-07-05 16:46:47 +0900

    update client.txtからclient_idとclient_secretを取得

--- app/src/main/java/com/pvcresin/girhubsearch/MainActivity.kt
+++ app/src/main/java/com/pvcresin/girhubsearch/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.pvcresin.girhubsearch
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ClientData(this)
+    }
+}
+
+class ClientData(context: Context) {
+    var id: String
+    var secret: String
+    init {
+        val lines = context.assets.open("client.txt").reader().readLines()
+//        lines.forEach { Log.d(this.javaClass.name, it) }
+        id = lines[1]
+        secret = lines[3]
     }
 }
